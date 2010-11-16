d3a9addff8931c12034908b14d9f0743aa902e55
Justin Davis <jrcd83@gmail.com>
2010-11-15 17:12:01 -0800

    Refactor pkg.h macros to limit code copy/pasting.

--- lib/pkg.h
+++ lib/pkg.h
@@ -1,42 +1,25 @@
 #ifndef _OALPM_PKG_H
 #define _OALPM_PKG_H
 
-#define OALPM_PKG_GET_STR( NAME )                                       \
-    CAMLprim value oalpm_pkg_get_ ## NAME ( value package )             \
-    {                                                                   \
-        pmpkg_t * pkg;                                                  \
-        CAMLparam1( package );                                          \
-        pkg = Package_val( package );                                   \
-        CAMLreturn( caml_copy_string( alpm_pkg_get_ ## NAME ( pkg )));  \
-    }
-
-#define OALPM_PKG_GET_STRLIST( NAME )                           \
-    CAMLprim value oalpm_pkg_ ## NAME ( value package )         \
-    {                                                           \
-        pmpkg_t * pkg;                                          \
-        CAMLparam1( package );                                  \
-        pkg = Package_val( package );                           \
-        CAMLlocal1( list );                                     \
-        list = CAML_STR_LIST( alpm_pkg_ ## NAME ( pkg ));       \
-        CAMLreturn( list );                                     \
-    }
-
-#define OALPM_PKG_LONG( NAME )                                          \
+#define OALPM_PKG_ACCESSOR( NAME, CAML_VAL )                            \
     CAMLprim value oalpm_pkg_ ## NAME ( value package )                 \
     {                                                                   \
         pmpkg_t * pkg;                                                  \
         CAMLparam1( package );                                          \
         pkg = Package_val( package );                                   \
-        CAMLreturn( Val_long( alpm_pkg_ ## NAME ( pkg )));              \
+        CAMLreturn( CAML_VAL ( alpm_pkg_ ## NAME ( pkg )));             \
     }
 
-#define OALPM_PKG_HAS_BOOL( NAME )                              \
-    CAMLprim value oalpm_pkg_has_ ## NAME ( value package )     \
-    {                                                           \
-        pmpkg_t * pkg;                                          \
-        CAMLparam1( package );                                  \
-        pkg = Package_val( package );                           \
-        CAMLreturn( Val_bool( alpm_pkg_has_ ## NAME ( pkg )));   \
-    }
+#define OALPM_PKG_GET_STR( NAME ) \
+    OALPM_PKG_ACCESSOR( get_ ## NAME, caml_copy_string )
+
+#define OALPM_PKG_STRLIST( NAME ) \
+    OALPM_PKG_ACCESSOR( NAME, CAML_STR_LIST )
+
+#define OALPM_PKG_LONG( NAME ) \
+    OALPM_PKG_ACCESSOR( NAME, Val_long )
+
+#define OALPM_PKG_HAS_BOOL( NAME ) \
+    OALPM_PKG_ACCESSOR( has_ ## NAME, Val_bool )
 
 #endif
