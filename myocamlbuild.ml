bed8735e7bfeffebdbb5f313814206e11e6736a3
Justin Davis <jrcd83@gmail.com>
2011-01-11 13:08:52 -0800

    Remove -g flag from compile options.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -14,14 +14,13 @@ let _ =
            a dependency of that target. *)
         dep [ "ocaml" ; "link" ; "use_libalpm" ] [ "libalpm.a" ] ;
 
-        let link_specs = S [ A "-cclib" ; A "-lalpm" ; A "-g" ;
+        let link_specs = S [ A "-cclib" ; A "-lalpm" ;
                              (S (List.map (fun x -> A x) oalpm_stub_libs)) ]
         in
 
         (* Add the -lalpm linker flag and .o object files to the
            command when compiling libalpm.a so it is automatically used
            when the libalpm.a library is linked/created. *)
-
         flag [ "native" ; "ocaml" ; "link" ; "library" ]
           link_specs ;
 
@@ -29,6 +28,8 @@ let _ =
            byte-compiling *)
         flag [ "byte" ; "program" ; "ocaml" ; "link" ; "use_libalpm" ]
           (S [ A "-custom" ; link_specs ]) ;
+
+        (* We must use -custom because we link with C code. *)
 
     | _ -> ()
   end
